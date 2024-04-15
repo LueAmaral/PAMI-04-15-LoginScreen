e891deff9b18d3bbca05c3ebb0f778adf9981f88
Lue <lue.amaral@etec.sp.gov.br>
2024-04-15 20:31:32 -0300

    primeiro commit

---
+++ src/components/screen/input/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from './style';
+import { TextInput, TextInputProps } from 'react-native';
+import { PropsWithChildren } from 'react';
+
+export const Input: React.FunctionComponent<TextInputProps> = ({...otherProps}
+    : PropsWithChildren<TextInputProps>) => {
+    return (
+        <Container>
+            <TextInput placeholderTextColor="rgb(192,192,192)" {...otherProps} />
+        </Container>
+    );
+};
---
+++ src/components/screen/input/style.ts
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+import styled from "styled-components/native";
+
+export const Container = styled(View)`
+    background-color: #ffffff;
+    color: #f1f1f1;
+    padding: 10px;
+    paddingLeft: 20px;
+    paddingRight: 20px;
+    marginTop: 30px;
+    borderWidth: 1px;
+    borderRadius: 24px;
+    borderColor: #313131;
+    width: 300px;
+`;
